90885b063c36ff298a887058c18036585b6ec048
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2026-08-29 14:12:47 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='phone-ledger-ipad-v3';
-const FILES=['./','./index.html','./app.js','./style.css','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
