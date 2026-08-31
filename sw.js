5c9c365848f4c26095da986fb22adbe16d8aff2a
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2026-08-31 16:02:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='phone-ledger-ipad-v8';
-const FILES=['./','./index.html','./app.js?v=8','./style.css?v=8','./manifest.webmanifest?v=8','./icon-v4-180.png','./icon-v4-512.png'];
+const CACHE='phone-ledger-ipad-v9';
+const FILES=['./','./index.html','./app.js?v=9','./style.css?v=9','./manifest.webmanifest?v=9','./icon-v4-180.png','./icon-v4-512.png'];
 
 async function networkFirst(request){
   try{
