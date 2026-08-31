16a01b70adc78ee8646794c53d5ac03a3958c982
Yang Yu <75100383+MrBetta520@users.noreply.github.com>
2026-08-31 15:24:58 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='phone-ledger-ipad-v5';
-const FILES=['./','./index.html','./app.js','./style.css','./manifest.webmanifest','./icon-v4-180.png','./icon-v4-512.png'];
+const CACHE='phone-ledger-ipad-v6';
+const FILES=['./','./index.html','./app.js?v=6','./style.css?v=6','./manifest.webmanifest','./icon-v4-180.png','./icon-v4-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES))));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request))));
